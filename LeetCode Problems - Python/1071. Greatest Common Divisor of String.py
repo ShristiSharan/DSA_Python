cf2d87d48234ec173c18b68711213173ff659dd0
ShristiSharan <shristisharan05@gmail.com>
2023-02-03 16:08:57 +0530

    Changes in 1071LC

--- LeetCode Problems - Python/1071. Greatest Common Divisor of String.py
+++ LeetCode Problems - Python/1071. Greatest Common Divisor of String.py
@@ -1,15 +1,43 @@
-def gcdOfStrings(self, str1, str2):
+
+        # l=len(str2)
+        # store=str1[0:l]
+        # if store==str2:
+        #     output=str1[l:]
+        # else:
+        #     output = ""
+
+        # return(output)
+
+class Solution(object):
+    def gcdOfStrings(self, str1, str2):
         """
         :type str1: str
         :type str2: str
         :rtype: str
         """
-        l=len(str2)
-        store=str1[0:l]
-        if store==str2:
-            output=str1[l:]
+        # l=len(str2)
+        # store=str1[0:l]
+        # if store==str2:
+        #     output=str1[l:]
+        #     return(output)
+        # else:
+        #     return("")
+        if len(str1)<=len(str2):
+             temp = str1
         else:
-            output = ""
+            temp = str2
+        m = len(temp)
+        x = 1
+        res=[""]
+        while x<=m:
+            if m%x==0 and temp[:x] * (len(str1)//x) == str1 and temp[:x] * (len(str2)//x) == str2:
+                res.append(temp[:x])
+            x+=1
+        return res[-1]
 
-        return(output)
+
+
+        
+
+ 
 
